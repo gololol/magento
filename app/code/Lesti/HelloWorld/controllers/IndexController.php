b71a70e1f57ca72da9403505fd15766ba7775fa6
gololol <gololol@ukr.net>
2015-04-14 22:38:04 +0300

    adding project to repository

---
+++ app/code/Lesti/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class Lesti_HelloWorld_IndexContorller extends Mage_Core_Controller_Front_Action
+{
+	public function indexAtion()
+	{
+		acho 'Hello World!';
+	}
+}
---
+++ app/code/community/h/w/controllers/indexController.php
@@ -0,0 +1,8 @@
+<?php
+class h_w_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Hello World !!';
+    }
+}
